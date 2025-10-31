c0dfa0631e26164d16fb62b6540de508e13598d4
ChillerDragon <chillerdragon@gmail.com>
2025-10-31 10:47:03 +0100

    Check for being pinged

--- src/chillerbot_reply/chillerbot_reply.cpp
+++ src/chillerbot_reply/chillerbot_reply.cpp
@@ -30,14 +30,34 @@ bool CChillerBotReply::LineShouldHighlight(const char *pLine, const char *pName)
 
 bool CChillerBotReply::Reply(const CChillerBotReplyChatMessage *pMsg, char *pReplyBuf, size_t ReplyBufLen)
 {
+	int MsgLen = str_length(pMsg->m_pMessage);
+	int NameLen = 0;
+
+	const char *pName = m_Context.m_aOwnTees[0].m_pName;
+	const char *pClan = m_Context.m_aOwnTees[0].m_pClan;
+	const char *pDummyName = m_Context.m_aOwnTees[1].m_pName;
+	const char *pDummyClan = m_Context.m_aOwnTees[1].m_pClan;
+
+	if(LineShouldHighlight(pMsg->m_pMessage, pName))
+		NameLen = str_length(pName);
+	else if(m_Context.m_IsDummyConnected && LineShouldHighlight(pMsg->m_pMessage, pDummyName))
+		NameLen = str_length(pDummyName);
+
+	// ping without further context
+	if(MsgLen < NameLen + 2)
+	{
+		str_format(pReplyBuf, ReplyBufLen, "%s ?", pMsg->m_pAuthor);
+		return true;
+	}
+
 	if(!str_comp_nocase(pMsg->m_pMessage, "hellow"))
 	{
 		str_copy(pReplyBuf, "yellow", ReplyBufLen);
 		return true;
 	}
-	if(str_comp_nocase(pMsg->m_pMessage, "hellow"))
+	if(str_find(pMsg->m_pMessage, "hellow"))
 	{
-		str_copy(pReplyBuf, "yellow", ReplyBufLen);
+		str_copy(pReplyBuf, "yellow xd", ReplyBufLen);
 		return true;
 	}
 	return false;
