50fbea128d6e963e0e61f6855ae2e2b062709bb4
ChillerDragon <chillerdragon@gmail.com>
2026-06-27 10:06:37 +0200

    doc

--- include/chillerbot_reply/text_helper.h
+++ include/chillerbot_reply/text_helper.h
@@ -2,7 +2,25 @@
 
 namespace TextHelper
 {
+	/*
+		GetSuffixLen
+
+		if pStr ends with pSuffix return length of pSuffix
+	*/
 	int GetSuffixLen(const char *pStr, const char *pSuffix);
+
+	/*
+		StripSpacesAndPunctuationAndOwnName
+
+		If the input pStr "yourname: hello, world  ??! ? !?"
+		is given the pStripped will point to "hello, world"
+	*/
 	void StripSpacesAndPunctuationAndOwnName(const char *pStr, const char *pName, const char *pDummyName, char *pStripped, int SizeOfStripped);
+
+	/*
+		IsEmptyStr
+
+		returns true if pStr is null "" or " " or "			 "
+	*/
 	bool IsEmptyStr(const char *pStr);
 }
